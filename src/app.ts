d4973d84db02d059d026ff8e7e595a16f52fb93b
wuiancocos2d <wuiancocos2d@gmail.com>
2023-09-01 03:51:11 +0800

    开始接入登录

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,19 @@ import './tailwindcss.css'
 import './style/index.scss'
 import 'taro-ui-vue3/dist/style/index.scss'
 import {setupStore} from "@/store";
+import Taro from "@tarojs/taro";
 const App = createApp({
   onShow (options) {
     console.log('App onShow.')
   },
+  onLaunch(){
+    let extConfig = Taro.getExtConfigSync? Taro.getExtConfigSync(): {}
+    debugger
+  }
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
 setupStore(App)
+
+
+
 export default App
