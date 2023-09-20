a0f12ba43dcb99c7b673e1d0a3e996e974f5bde5
wuiancocos2d <wuiancocos2d@gmail.com>
2023-09-20 17:38:29 +0800

    自定义radio组件

---
+++ src/components/order/productOption/ShopCount.vue
@@ -0,0 +1,28 @@
+<template>
+  <view>
+    <view class="wrapper">
+      <div class="total">
+        总计{{amount}}
+      </div>
+
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+  name: "shop-count",
+  setup(){
+    const amount = ref(0)
+    return {
+      amount
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
